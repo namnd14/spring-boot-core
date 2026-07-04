096303361e139a47f0d67f03f1b097c4247c8ed6
namnd <namnguyennd7428@gmail.com>
2026-07-04 22:14:26 +0700

    feat: set default format for local date and local date time, using ISO 8601 format

--- src/main/java/com/spring/core/dto/request/RecordExperimentRequest.java
+++ src/main/java/com/spring/core/dto/request/RecordExperimentRequest.java
@@ -1,7 +1,5 @@
 package com.spring.core.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -9,9 +7,7 @@ public record RecordExperimentRequest(
         Integer id,
         Long idLong,
         String name,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         LocalDate date,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime dateTime,
         Boolean isActive
 ) {
